b1d400bb6c051055cd04d174730562a54ae248bb
EC2 Default User <ec2-user@ip-172-31-42-138.ap-northeast-1.compute.internal>
2020-12-20 13:26:26 +0000

    新規投稿のバックグラウン作成ド

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,16 @@ class ReviewsController < ApplicationController
     end
     
     def new
+       @review=Review.new 
+    end
+    
+    def create
+        Review.create(review_params)
+    end
+    
+    private
+    def review_params
+        params.require(:review).permit(:image, :text, :title, :rate)
     end
     
 end
