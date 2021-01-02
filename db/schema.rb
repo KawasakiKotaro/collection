8adbfc7de5f470aa3da81d69d15941a99e417f48
EC2 Default User <ec2-user@ip-172-31-42-138.ap-northeast-1.compute.internal>
2021-01-02 06:46:09 +0000

    commentモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_144834) do
+ActiveRecord::Schema.define(version: 2020_12_31_132349) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
